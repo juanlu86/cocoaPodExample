d4be1d600f4f31b077abf4ef8d917d4b75159d65
Juanlu <juanlu.lopez.munoz@gmail.com>
2014-05-02 17:23:31 +0200

    Example class added

---
+++ Classes/ios/podExampleClass.h
@@ -0,0 +1,13 @@
+//
+//  podExampleClass.h
+//  CocoaPodsExample
+//
+//  Created by Juanlu on 02/05/14.
+//  Copyright (c) 2014 nspyre. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface podExampleClass : NSObject
+
+@end
---
+++ Classes/ios/podExampleClass.m
@@ -0,0 +1,13 @@
+//
+//  podExampleClass.m
+//  CocoaPodsExample
+//
+//  Created by Juanlu on 02/05/14.
+//  Copyright (c) 2014 nspyre. All rights reserved.
+//
+
+#import "podExampleClass.h"
+
+@implementation podExampleClass
+
+@end
